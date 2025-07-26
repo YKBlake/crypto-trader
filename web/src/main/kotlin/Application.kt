3d95e076ec3192c5a98ccf75de52aca41c8f0f6f
YKB <kaanboydak@yahoo.com>
2025-07-26 15:37:49 +0300

    Web microservice added

--- web/src/main/kotlin/Application.kt
+++ web/src/main/kotlin/Application.kt
@@ -1,4 +1,4 @@
-package com.ykb.app.cryptotrader
+package com.ykb.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
