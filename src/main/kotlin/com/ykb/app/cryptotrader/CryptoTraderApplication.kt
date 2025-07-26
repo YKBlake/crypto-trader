fba40fad023b8c7f4287035fd393c32e7199b3ab
YKBlake <69684201+YKBlake@users.noreply.github.com>
2025-07-26 14:52:02 +0300

    initial push

---
+++ src/main/kotlin/com/ykb/app/cryptotrader/CryptoTraderApplication.kt
@@ -0,0 +1,11 @@
+package com.ykb.app.cryptotrader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoTraderApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoTraderApplication>(*args)
+}
