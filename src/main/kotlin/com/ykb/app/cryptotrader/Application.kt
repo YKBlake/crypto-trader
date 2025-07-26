6e297225217180c77301046c3d05b2ad69ec876e
YKB <kaanboydak@yahoo.com>
2025-07-26 15:21:00 +0300

    Vite+React integration

--- src/main/kotlin/com/ykb/app/cryptotrader/Application.kt
+++ src/main/kotlin/com/ykb/app/cryptotrader/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CryptoTraderApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<CryptoTraderApplication>(*args)
+	runApplication<Application>(*args)
 }
